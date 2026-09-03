7ef56c756d07b01f0cea277b5c40fb319426ac03
Prince Mahamud <pallensteve105@gmail.com>
2026-09-03 21:11:06 +0600

    feat: configure production api routing

--- servicehub-frontend/src/services/api.js
+++ servicehub-frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     process.env.REACT_APP_API_URL ||
-    "http://localhost:5000/api",
+    "/api",
 });
 
 api.interceptors.request.use(
